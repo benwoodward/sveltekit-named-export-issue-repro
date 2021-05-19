fc53cc8b49fb3cb1342c92e088d3a4c992363e6c
Ben Woodward <1472981+benwoodward@users.noreply.github.com>
2021-05-20 10:38:18 +1200

    chore: Add more examples

--- src/lib/Thing.svelte
+++ src/lib/Thing.svelte
@@ -1,5 +1,7 @@
 <script>
-  import { waitUntil } from 'async-wait-until'
+  import waitUntil from 'async-wait-until' // works
+  // import { waitUntil } from 'async-wait-until' // named export error
+  // import { waitUntil } from 'async-wait-until/dist/index.esm.js'; // named export error
 
   const task = async () => {
     await waitUntil(
